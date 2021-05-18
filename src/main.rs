a1b07576be849ef4b0b5a9ed50f60547580ca1f5
Seaborn Lee <seabornlee.cn@gmail.com>
2021-05-18 08:00:05 +0800

    feat: mpsc demo

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,28 @@
 use bytes::Bytes;
-use mini_redis::{Connection, Frame};
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use tokio::net::{TcpListener, TcpStream};
 
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+#[derive(Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, val: Bytes },
+}
+
+
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    println!("Listening");
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
 
-    let db = Arc::new(Mutex::new(HashMap::new()));
+    tokio::spawn(async move {
+        tx.send("sending from first handle").await;
+    });
 
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        let db = db.clone();
+    tokio::spawn(async move {
+        tx2.send("sending from the second handle").await;
+    });
 
-        println!("Accepted");
-        tokio::spawn(async move {
-            process(socket, db).await;
-        });
-    }
-}
-
-async fn process(socket: TcpStream, db: Db) {
-    use mini_redis::Command::{self, Get, Set};
-
-    let mut connection = Connection::new(socket);
-    while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response = match Command::from_frame(frame).unwrap() {
-            Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                db.insert(cmd.key().to_string(), cmd.value().clone());
-                Frame::Simple("OK".to_string())
-            }
-
-            Get(cmd) => {
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone())
-                } else {
-                    Frame::Null
-                }
-            }
-
-            cmd => panic!("unimplemented {:?}", cmd),
-        };
-        connection.write_frame(&response).await.unwrap();
+    while let Some(message) = rx.recv().await {
+        println!("GOT = {}", message);
     }
 }
